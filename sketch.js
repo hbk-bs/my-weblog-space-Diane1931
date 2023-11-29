631e20ffc00c0b3879a7ee0406cdaa5e672d9564
Diane1931 <m.jacobson@hbk-bs.de>
2023-11-29 14:38:01 +0100

    Interactive Color Experiment

--- sketch.js
+++ sketch.js
@@ -1,12 +1,34 @@
+const saveButton = document.querySelector ('button#save');
+const colorInput= document.querySelector ('input#color');
+console.log (colorInput);
+
+let paintColor= "#ff6347";
+colorInput.value=paintColor;
+
 function setup ()
 {
-createCanvas(500,500);
+const canvas= createCanvas(500,500);
+canvas.parent('sketch');
+background(255);
+
+saveButton.addEventListener('click',()=> {
+console.log ("clicked");
+save("image.png");
+
+
+
+});
+colorInput.addEventListener("input", () =>{
+    paintColor=colorInput.value;
+
+})
+
 }
 
 function draw (){}
-background (0)
 
 function mouseDragged (){
+    fill(paintColor);
 circle (mouseX,mouseY,10);
 line (pmouseX, pmouseY,mouseX, mouseY);
 for(let i=0; i<100; i++) {
@@ -15,6 +37,13 @@ point(
     mouseY+ random (-10,10));
     }
     }
+
+function buttonHandler() {
+    save('image.png');  
+
+    }
+
+    
 
 function keyPressed()  {
     if(key==='s')
